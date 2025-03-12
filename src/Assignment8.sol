1341b26590fe10cf533dd4e3c19bf35b2c8befde
Omar Osman <omarosman23@aucegypt.edu>
2025-03-13 00:33:19 +0200

    :fire: feat: add erc721 uri storage

--- src/Assignment8.sol
+++ src/Assignment8.sol
@@ -2,21 +2,30 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";    
+import "@openzeppelin/contracts/token/access/Ownable.sol";
 
-contract Assignment8 is ERC721 {
-    uint256 private _tokenIdCounter;
+// make this contract inherits from: ERC721URIStorage contract
+contract Assignment8 {
+    // declare a private uint called _tokenIdCounter
 
-    // Constructor to initialize the NFT
-    // Fill the the token name and symbol to the ERC721 constructor
-    constructor() ERC721() {}
-
-    // Function to mint an NFT
-    // This function can only be called by the owner
-    // Make the owner of the contract is the only user that call this mint function
-        // Hint: You can either create your own onlyOwner modifier, or you use Openzeppelin Ownable contract
-    // Fill in the address param
-    function mintNFT() external onlyOwner {
-        // increment _tokenIdCounter counter
-        // call _mint using to and _tokenIdCounter params
+    // pass name and symbol of the nft token collection
+    constructor() ERC721() {
+        
     }
+
+    // make this function visibility: external
+    // pass string json uri parameter
+    // restrict this function to be called only by the owner
+        // Hint: You can use OpenZeppelin Ownable contract imported above
+    
+    function mintNFT() {
+        // call _mint to mint a new nft to the function caller
+
+        // set token uri to the token id using _setTokenURI
+
+        // increment token id counter
+
+    }
+
 }
